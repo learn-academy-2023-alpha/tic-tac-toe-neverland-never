e84da9d9cb28b38ea03af607aaf1614a95c4b695
LEARN Academy <learnacademy@LEARNs-MacBook-Air.local>
2023-03-07 10:48:31 -0800

    Added on a Onclick and marked

--- src/App.js
+++ src/App.js
@@ -6,14 +6,17 @@ const App = () => {
       // Create a state to track the gameboard
       const [squares, setSquares] = useState(Array(9).fill(null))
 
+      const processClick= () => {
+        alert("click detected")
+      }
   return (
     <>
       <div className='header'>
             <h1>Neverland Never</h1>
       </div>
       <div className='gameboard'>
-            {squares.map((value, index) => <Square />)}
-            {/*  show={value} key={index} index={index} processClick={processClick}/>)} */}
+            {squares.map((value, index) => <Square show={value} key={index} processClick={processClick}/>)}  
+            {/*   index={index} */}
       </div>
     </>
   )
