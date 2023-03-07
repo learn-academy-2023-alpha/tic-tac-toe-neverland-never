b8e7a769fe4475611aa2f35a75f2dc30b1581109
csaument <c.s.aument@gmail.com>
2023-03-07 10:28:47 -0800

    created 3x3 render of squares in a gamebox

--- src/App.js
+++ src/App.js
@@ -3,12 +3,18 @@ import Square from './components/Square'
 import './App.css'
 
 const App = () => {
-  const [squares, setSquares] = useState(Array(9).fill(null))
+      // Create a state to track the gameboard
+      const [squares, setSquares] = useState(Array(9).fill(null))
 
   return (
     <>
-      <h1>Tic Tac Toe</h1>
-      <Square />
+      <div className='header'>
+            <h1>Neverland Never</h1>
+      </div>
+      <div className='gameboard'>
+            {squares.map((value, index) => <Square />)}
+            {/*  show={value} key={index} index={index} processClick={processClick}/>)} */}
+      </div>
     </>
   )
 }
